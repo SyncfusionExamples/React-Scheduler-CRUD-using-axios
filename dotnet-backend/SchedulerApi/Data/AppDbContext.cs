328d1aa05742a5be294eed061d0bdcc50daddb40
Syed Anas Nizam Deen <syedanas.nizamdeen@gmail.com>
2026-02-27 17:10:45 +0530

    updated backend pushed to repo

---
+++ dotnet-backend/SchedulerApi/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchedulerApi.Models;
+
+namespace SchedulerApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ScheduleEventData> ScheduleEventDatas => Set<ScheduleEventData>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var e = modelBuilder.Entity<ScheduleEventData>();
+            e.ToTable("ScheduleEventDatas");
+            e.Property(x => x.Id)
+              .ValueGeneratedNever(); // app provides Id
+
+            // Map to datetimeoffset in SQL Server
+            e.Property(x => x.StartTime).HasColumnType("datetimeoffset");
+            e.Property(x => x.EndTime).HasColumnType("datetimeoffset");
+        }
+    }
+}
---
+++ dotnet-backend/SchedulerApi/Migrations/20260216100608_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchedulerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduleEventDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    EndTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    StartTimezone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EndTimezone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsAllDay = table.Column<bool>(type: "bit", nullable: false),
+                    RecurrenceRule = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecurrenceID = table.Column<int>(type: "int", nullable: true),
+                    RecurrenceException = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleEventDatas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleEventDatas");
+        }
+    }
+}
